76c112f453bafe0ec4cd30078ffad2bbb65fb66c
Andrew Kabanov <kbnandrii@gmail.com>
2024-07-02 18:27:01 +0300

    correction of comments

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8000",
 ]
