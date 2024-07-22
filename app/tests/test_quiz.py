2e2e3f81d987ac47115f85e2a13d8e2ba7f016f3
Andrew Kabanov <kbnandrii@gmail.com>
2024-07-22 15:14:32 +0300

    upd

--- app/tests/test_quiz.py
+++ app/tests/test_quiz.py
@@ -1,18 +1,13 @@
 import pytest
 from unittest.mock import AsyncMock
-from datetime import datetime
 
 from app.schemas.quiz import (
     QuizCreate,
-    QuizUpdate,
     QuizBase,
-    QuizResponse,
-    QuizzesListResponse,
 )
 from app.services.quiz import QuizService
 from app.uow.unitofwork import UnitOfWork
 from app.exceptions.auth import UnAuthorizedException
-from app.exceptions.base import NotFoundException
 
 
 @pytest.mark.asyncio
@@ -28,20 +23,6 @@ async def test_create_quiz():
 
     with pytest.raises(UnAuthorizedException):
         await QuizService.create_quiz(mock_uow, quiz_data, current_user_id=1)
-
-
-@pytest.mark.asyncio
-async def test_get_quizzes():
-    mock_uow = AsyncMock(UnitOfWork)
-    mock_uow.quiz = AsyncMock()
-    mock_uow.member = AsyncMock()
-
-    company_id = 1
-    mock_quizzes = [QuizBase(id=1, title="Test Quiz", company_id=1)]
-    mock_uow.quiz.find_all.return_value = mock_quizzes
-
-    with pytest.raises(UnAuthorizedException):
-        await QuizService.get_quizzes(mock_uow, company_id, current_user_id=1)
 
 
 @pytest.mark.asyncio
