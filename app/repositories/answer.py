3166827350b9ee9091825667c367246c1c02ad9b
Andrew Kabanov <kbnandrii@gmail.com>
2024-07-23 12:26:56 +0300

    upd from previous branch

--- app/repositories/answer.py
+++ app/repositories/answer.py
@@ -8,6 +8,6 @@ class AnswerRepository(SQLAlchemyRepository):
     model = Answer
 
     async def find_all_by_question_id(self, question_id: int):
-        stmt = select(self.model).filter_by(question_id=question_id)
+        stmt = select(self.model).where(self.model.question_id == question_id)
         res = await self.session.execute(stmt)
         return res.scalars().all()
