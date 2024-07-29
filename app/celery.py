1c289d42b23e508fc0a2d29819978480d3cc2565
Andrew Kabanov <kbnandrii@gmail.com>
2024-07-30 01:15:06 +0300

    tests

--- app/celery.py
+++ app/celery.py
@@ -1,16 +1,20 @@
 import nest_asyncio
 import asyncio
-
 from celery import Celery
 from celery.schedules import crontab
-
 from app.core.config import settings
 from app.core.tasks import notification_task
 
 nest_asyncio.apply()
 
+broker_use_ssl = {
+    "ssl_cert_reqs": "CERT_NONE",
+}
+
 celery = Celery(
-    "tasks", broker=f"redis://{settings.redis.host}:{settings.redis.port}/0"
+    "tasks",
+    broker=f"rediss://{settings.redis.host}:{settings.redis.port}/0",
+    broker_use_ssl=broker_use_ssl,
 )
 
 
